3de08ec9ea96cf0ba4bfd74838abc25c01edea1a
tamadam <tamadam9@gmail.com>
2024-05-02 19:41:58 +0200

    Retrieve searchQuery in the page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Search from "./components/Search/Search";
 
-export default function SearchPage() {
+interface SearchPageProps {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+const SearchPage = ({ searchParams }: SearchPageProps) => {
+  const searchQuery = searchParams["query"]?.[0] ?? "";
+
+  console.log(searchQuery);
+
   return (
     <main className="flex flex-col gap-4 items-center justify-center min-h-svh">
       <Search />
     </main>
   );
-}
+};
+
+export default SearchPage;
